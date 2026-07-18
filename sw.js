346f47cf0194e058c77eb663cdad8a69fe880637
Osborn4278 <lorenz@loryle.com>
2026-07-18 23:13:25 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,21 @@
-self.addEventListener('fetch', function(event) {
-  // Lasciamo passare tutte le richieste normalmente
-});
+self.addEventListener('install', e => {
+ console.log('PWA Service Worker installing.');
+ let timeStamp = Date.now();
+ e.waitUntil( caches.open('evemilano_service_worker').then(cache => { 
+return cache.addAll([ 
+
+
+ ])
+ .then(() => self.skipWaiting());
+ })
+ )
+ });
+ self.addEventListener('activate', event => { 
+console.log('PWA Service Worker activating.');
+ event.waitUntil(self.clients.claim()); });
+ self.addEventListener('fetch', event => {
+ event.respondWith( caches.match(event.request).then(response => { 
+return response || fetch(event.request); 
+})
+ );
+ });
